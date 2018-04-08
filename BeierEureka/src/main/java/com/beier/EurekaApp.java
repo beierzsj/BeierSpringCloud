040a85eb638dac656bd6ef820930a05cfce68e94
倾愿逆仙 <940013154@qq.com>
2018-04-08 17:35:20 +0800

    Initial commit-Eureka搭建完成

---
+++ BeierEureka/src/main/java/com/beier/EurekaApp.java
@@ -0,0 +1,18 @@
+package com.beier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka启动
+ * @author hua
+ * @create 2018/4/8.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class,args);
+    }
+}
